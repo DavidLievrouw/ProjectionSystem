3783fdcf0f4382659fa240319b03849e3e32ad09
David Lievrouw <dlievrouw2@csc.com>
2016-06-07 12:24:49 +0200

    Add unit tests for RealSyncLock and RealSyncLockFactory.

---
+++ src/ProjectionSystem.Tests/RealSyncLockFactoryTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DavidLievrouw.Utils.ForTesting.FluentAssertions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ProjectionSystem {
+  [TestFixture]
+  public class RealSyncLockFactoryTests {
+    SemaphoreSlim _semaphore;
+    RealSyncLockFactory _sut;
+
+    [SetUp]
+    public void SetUp() {
+      _sut = new RealSyncLockFactory(_semaphore);
+    }
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp() {
+      _semaphore = new SemaphoreSlim(1);
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown() {
+      _semaphore.Dispose();
+    }
+
+    [TestFixture]
+    public class Construction : RealSyncLockFactoryTests {
+      [Test]
+      public void HasExactlyOneConstructor_WithNoOptionalParameters() {
+        _sut.Should().HaveExactlyOneConstructorWithoutOptionalParameters();
+      }
+    }
+
+    [TestFixture]
+    public class Create : RealSyncLockFactoryTests {
+      [Test]
+      public async Task CreatesDisposableLock() {
+        using (var actual = await _sut.Create()) {
+          Assert.That(actual, Is.InstanceOf<RealSyncLock>());
+        }
+      }
+    }
+  }
+}
---
+++ src/ProjectionSystem.Tests/RealSyncLockTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace ProjectionSystem {
+  [TestFixture]
+  public class RealSyncLockTests {
+    SemaphoreSlim _semaphore;
+    RealSyncLock _sut;
+
+    [SetUp]
+    public void SetUp() {
+      _sut = new RealSyncLock(_semaphore);
+    }
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp() {
+      _semaphore = new SemaphoreSlim(1);
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown() {
+      _semaphore.Dispose();
+    }
+
+    [TestFixture]
+    public class Construction : RealSyncLockTests {
+      [Test]
+      public void HasExactlyOneConstructor_WithNoOptionalParameters() {
+        Assert.Throws<ArgumentNullException>(() => new RealSyncLock(null));
+      }
+    }
+
+    // Interface extracted code.
+    // Useless to test the .NET SemaphoreSlim implementation.
+  }
+}
